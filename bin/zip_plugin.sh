4d22d22aa88e0e9ad6b9eac58da51e6401e8c476
Antonio Kamiya <kamiya@magaport.co.jp>
2019-10-28 06:49:13 +0000

    update deployment batch

--- bin/zip_plugin.sh
+++ bin/zip_plugin.sh
@@ -1,1 +1,3 @@
+ rm -f ../articlecms-wp_plugin.zip
  zip -r ../articlecms-wp_plugin.zip . -x *.git*
+ scp -i ~/.ssh/Keys/fujisan/id_kamiya_2015 ../articlecms-wp_plugin.zip bitnami@articles.magaport.co.jp:
